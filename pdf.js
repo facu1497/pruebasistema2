76800b21b3e2ca91ff5acd8c0fb879623250420c
facu1497 <63.tad.haggard@icloud.com>
2025-12-27 21:41:52 -0300

    Add files via upload

---
+++ pdf.js
@@ -0,0 +1,5 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js"></script>
+<script>
+  // Configuración del worker de PDF.js
+  pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
+</script>
