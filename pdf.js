fcb71c7e4aa83ab3c2a8820d12d79b9cd638ca37
facu1497 <63.tad.haggard@icloud.com>
2025-12-27 23:47:46 -0300

    Delete pdf.js

--- pdf.js
+++
@@ -1,5 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js"></script>
-<script>
-  // Configuración del worker de PDF.js
-  pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
-</script>
